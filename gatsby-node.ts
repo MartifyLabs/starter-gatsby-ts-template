be0e520d6e61e75a7127c33728e41cf720db41df
Abdelkrim Dib <abdelkrim.dib@icloud.com>
2022-11-13 15:20:06 +0100

    update project config

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -1,7 +1,7 @@
 exports.onCreateWebpackConfig = ({ actions }) => {
   actions.setWebpackConfig({
     experiments: {
-      syncWebAssembly: true,
+      asyncWebAssembly: true,
     },
     resolve: {
       fallback: {
